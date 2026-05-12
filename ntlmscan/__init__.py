d889d324583e24856dd07cf3acf1276adc9274ce
Coontzy1 <coontz.austin@gmail.com>
2026-05-11 23:58:47 -0400

    Add pyproject.toml for pip/pipx/uv install support

---
+++ ntlmscan/__init__.py
@@ -0,0 +1,4 @@
+# NTLM scanner - finds HTTP endpoints that advertise NTLM auth
+from ntlmscan.__main__ import main
+
+__all__ = ["main"]
--- ntlmscan/__main__.py
+++ ntlmscan/__main__.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-
-
 # 2019.10.22 - @nyxgeek - TrustedSec
-#
-# NTLM scanner - just looks for HTTP header that specifies NTLM auth
-# takes a url, or a list of hosts
+# NTLM scanner - finds HTTP header that specifies NTLM auth
 
 from queue import Queue
 import threading
@@ -17,13 +13,30 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from urllib.parse import urlparse
 import os
 
-dictionaryfile = "paths.dict"
 outputfile = "output.log"
+dictionaryfile = "paths.dict"
 debugoutput = False
 nmapscan = False
 foundURLs = []
 add_lock = threading.Lock()
 queue = Queue()
+
+
+def _default_dictionary_path():
+    """Resolve bundled paths.dict (works when installed via pip/pipx)."""
+    try:
+        from importlib.resources import files as _resource_files
+        path = _resource_files("ntlmscan") / "paths.dict"
+        if path.is_file():
+            return str(path)
+    except Exception:
+        pass
+    try:
+        import pkg_resources
+        return pkg_resources.resource_filename("ntlmscan", "paths.dict")
+    except Exception:
+        pass
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "paths.dict")
 
 
 def process_queue():
@@ -34,18 +47,15 @@ def process_queue():
 
 
 def nmapScanner(foundURLs):
-    # if nmap was selected, let's do some scans
     for targeturl in foundURLs:
         print("Initializing nmap scan for {}".format(targeturl))
         parsedURL = urlparse(targeturl)
         targethost = parsedURL.hostname
         targetpath = parsedURL.path
-        # use explicit port from URL if present, else default by scheme
         port = parsedURL.port
         if port is None:
             port = 80 if parsedURL.scheme == "http" else 443
         print("host:\t{host}\npath:\t{path}\nport:\t{port}".format(host=targethost, path=targetpath, port=port))
-
         nmapcmd = "nmap -Pn -sT -p{port} --script=http-ntlm-info --script-args=http-ntlm-info.root={path} {host}".format(
             port=port, path=targetpath, host=targethost
         )
@@ -55,7 +65,6 @@ def nmapScanner(foundURLs):
 def makeRequests(url_data):
     global foundURLs
     url, force_ntlm, virtualhost = url_data
-    # print("\r[-] Testing path {}".format(url), end='')
     with add_lock:
         print("[-] Testing path {}".format(url))
     try:
@@ -64,11 +73,7 @@ def makeRequests(url_data):
         else:
             headers = {}
         if force_ntlm:
-            # This checks for forced NTLM, which would show positive on every URL on the site
-            headers[
-                "Authorization"
-            ] = "NTLM TlRMTVNTUAABAAAAB4IIAAAAAAAAAAAAAAAAAAAAAAA="
-
+            headers["Authorization"] = "NTLM TlRMTVNTUAABAAAAB4IIAAAAAAAAAAAAAAAAAAAAAAA="
         r = requests.head(url, timeout=3, headers=headers, verify=False)
         if debugoutput:
             print(r.headers)
@@ -81,22 +86,19 @@ def makeRequests(url_data):
                     else:
                         print("[+] FOUND NTLM - {}".format(url))
                     foundURLs.append(url)
-                    # here we open the file quick to write to it - we might want to relocate this open/close to outside here
                     with open(outputfile, "a") as outfilestream:
                         if force_ntlm:
                             outfilestream.write(f"[+] FOUND FORCED NTLM - {url}\n")
                         else:
                             outfilestream.write("[+] FOUND NTLM - {}\n".format(url))
     except requests.exceptions.ReadTimeout:
-        # print("\r", end='')
         pass
-
     except Exception:
-        # print("Unexpected error:", sys.exc_info()[0])
         pass
 
 
-if __name__ == "__main__":
+def main():
+    global outputfile, dictionaryfile, debugoutput
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="full url path to test")
     parser.add_argument("--host", help="a single host to search for ntlm dirs on")
@@ -109,47 +111,33 @@ if __name__ == "__main__":
     parser.add_argument("--outfile", help="file to write results to")
     parser.add_argument(
         "--dictionary",
-        help="list of paths to test, default: paths.dict",
-        default=os.path.dirname(os.path.abspath(__file__)) + "/paths.dict",
+        help="list of paths to test, default: paths.dict (bundled when installed via pip/pipx)",
+        default=_default_dictionary_path(),
     )
-    parser.add_argument(
-        "--nmap", help="run nmap when complete", action="store_true", default=False
-    )
-    parser.add_argument(
-        "--debug", help="show request headers", action="store_true", default=False
-    )
-    parser.add_argument(
-        "--threads", help="Number of threads to use Default 100", type=int, default=100
-    )
-    parser.add_argument(
-        "--http", help="use HTTP instead of HTTPS when constructing URLs", action="store_true", default=False
-    )
-    parser.add_argument(
-        "--both", help="try both HTTP and HTTPS for each path", action="store_true", default=False
-    )
+    parser.add_argument("--nmap", help="run nmap when complete", action="store_true", default=False)
+    parser.add_argument("--debug", help="show request headers", action="store_true", default=False)
+    parser.add_argument("--threads", help="Number of threads to use Default 100", type=int, default=100)
+    parser.add_argument("--http", help="use HTTP instead of HTTPS when constructing URLs", action="store_true", default=False)
+    parser.add_argument("--both", help="try both HTTP and HTTPS for each path", action="store_true", default=False)
     args = parser.parse_args()
 
     if args.http and args.both:
         parser.error("--http and --both are mutually exclusive")
 
-    # print help if no host arguments are supplied
     if not (args.url or args.host or args.hostfile or args.urlfile):
         parser.print_help()
         quit(1)
-    # check to see if a custom outfile has been specified
+
     if args.outfile:
         print("Output file set to {}".format(args.outfile))
         outputfile = args.outfile
 
-    # check to see if a custom dictionary is set
     if args.dictionary:
         print("custom dictionary has been set to {}".format(args.dictionary))
         dictionaryfile = args.dictionary
-        if not os.path.isfile(dictionaryfile):
-            dictionaryfile = os.path.dirname(__file__) + args.dictionary
+        if not os.path.isfile(dictionaryfile) and not os.path.isabs(args.dictionary):
+            dictionaryfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.dictionary)
 
-    # now that we have that sorted, load the dictionary into array called pathlist
-    # print("Using dictionary located at: {}".format(dictionaryfile))
     with open(dictionaryfile, "r") as pathdict:
         pathlist = pathdict.readlines()
 
@@ -159,7 +147,6 @@ if __name__ == "__main__":
     if args.nmap:
         nmapscan = True
 
-    # Scheme selection: default HTTPS, or HTTP with --http, or both with --both (raw hosts only)
     if args.both:
         schemes = ["http", "https"]
     elif args.http:
@@ -167,10 +154,8 @@ if __name__ == "__main__":
     else:
         schemes = ["https"]
 
-    ## NOW, HERE ARE THE MAIN WORKHORSE FUNCTION CALLS ##
-
     if args.url:
-        queue.put([args.url, False])
+        queue.put([args.url, False, None])
 
     if args.host:
         host_has_scheme = args.host[:4] == "http" if len(args.host) >= 4 else False
@@ -185,7 +170,6 @@ if __name__ == "__main__":
                 for scheme in schemes:
                     testurl = scheme + "://" + args.host + "/" + urlpath
                     queue.put([testurl, False, args.virtualhost])
-
         if host_has_scheme:
             testurl = args.host.rstrip("/") + "/"
             queue.put([testurl, True, args.virtualhost])
@@ -193,14 +177,13 @@ if __name__ == "__main__":
             for scheme in schemes:
                 testurl = scheme + "://" + args.host + "/"
                 queue.put([testurl, True, args.virtualhost])
+
     if args.hostfile:
         with open(args.hostfile, "r") as hostfile:
             hostlist = hostfile.readlines()
-
         for hostname in hostlist:
             hostname = hostname.rstrip()
             host_has_scheme = hostname[:4] == "http" if len(hostname) >= 4 else False
-
             for urlpath in pathlist:
                 urlpath = urlpath.rstrip()
                 if urlpath.startswith("/"):
@@ -216,7 +199,6 @@ if __name__ == "__main__":
     if args.urlfile:
         with open(args.urlfile, "r") as f:
             urllist = [line.rstrip() for line in f if line.strip()]
-
         for baseurl in urllist:
             for urlpath in pathlist:
                 urlpath = urlpath.rstrip()
@@ -224,10 +206,8 @@ if __name__ == "__main__":
                     urlpath = urlpath[1:]
                 testurl = baseurl.rstrip("/") + "/" + urlpath
                 queue.put([testurl, False, args.virtualhost])
-            # forced NTLM root check for this base URL
             queue.put([baseurl.rstrip("/") + "/", True, args.virtualhost])
 
-    # Get ready to queue some requests
     for i in range(args.threads):
         t = threading.Thread(target=process_queue)
         t.daemon = True
@@ -239,7 +219,6 @@ if __name__ == "__main__":
     if args.nmap:
         nmapScanner(foundURLs)
 
-    # Print summary of all found NTLM URLs
     if foundURLs:
         print("\n" + "=" * 50)
         print("NTLM URLs Found:")
@@ -250,3 +229,7 @@ if __name__ == "__main__":
         print(f"Total: {len(foundURLs)} URL(s)")
     else:
         print("\nNo NTLM URLs found.")
+
+
+if __name__ == "__main__":
+    main()
